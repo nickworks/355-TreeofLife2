9684e0e81769eb30126a60310796d5add445d295
Jacob Wiles <Awsoemman328@gmail.com>
2019-10-31 16:17:28 -0400

    Completed Basic Boss State Machine Scripts
    +Created Scripts Folder and Boss Behavior Folder inside of that.
    +Created Scripts for: BossController, BossState, BossStateIdle, BossStatePursue, & BossStateAttack. The controller dictates which states are currently active, the BosState is a parent class to all of the other states, and the other three states contain the boss's current behavior and how to transition between these states.

---
+++ Assets/Wiles/Scripts/BossBehavior/WilesBossController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wiles
+{
+    public class WilesBossController : MonoBehaviour
+    {
+
+        public float visionDistanceThreshold = 10;
+        public float pursueDistanceThreshold = 7;
+        public float speed = 10;
+
+        [HideInInspector]
+        public Vector3 velocity = new Vector3();
+
+        WilesBossState currentState;
+
+        public Transform attackTarget { get; private set; }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            if(player !=null) attackTarget = player.transform;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (currentState == null) SwitchToState( new WilesBossStateIdle());
+            if (currentState != null) SwitchToState(currentState.Update(this));
+
+        }
+
+        private void SwitchToState(WilesBossState newState)
+        {
+            if (newState != null)
+            {
+                if(currentState != null) currentState.OnEnd(this);
+                currentState = newState;
+                currentState.OnStart(this);
+            }
+        }
+
+        public Vector3 VectorToAttackTarget()
+        {
+            return attackTarget.position - transform.position;
+        }
+
+        public float DistanceToAttackTarget()
+        {
+            return VectorToAttackTarget().magnitude;
+
+        }
+
+        public bool CanSeeAttackTarget()
+        {
+            // if dis < threshold && raycast from player to boss hits...
+            // transition to pursue
+            if (attackTarget == null) return false;
+            Vector3 vectorBetween = VectorToAttackTarget();
+
+            if (vectorBetween.sqrMagnitude < visionDistanceThreshold * visionDistanceThreshold)
+            {
+                // player is close enough to boss to activate it...
+                Ray ray = new Ray(transform.position, vectorBetween.normalized);
+                // RaycastHit hit;
+                if (Physics.Raycast(ray, out RaycastHit hit))
+                {
+                    //Debug.Log("Raycast from boss to player hit something!!");
+                    if (hit.transform == attackTarget) return true; // clear line of vision!
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Assets/Wiles/Scripts/BossBehavior/WilesBossStateIdle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wiles
+{
+    public class WilesBossStateIdle : WilesBossState
+    {
+        public override WilesBossState Update(WilesBossController boss)
+        {
+            // do stuff to the boss...
+
+            Debug.Log(this);
+
+            // transitions to other states:
+
+
+            if (boss.CanSeeAttackTarget())
+            {
+                // transition to pursure state...
+                return new WilesBossStatePursue();
+            }
+
+            return null; // stay in current state
+        }
+
+       
+
+        public override void OnStart(WilesBossController boss)
+        {
+            // base.OnStart(boss);
+
+
+
+        }
+
+    }
+}
