2e970748351c852d1819d5d1f315ca2afd80c79e
Keegan <johnk117@ferris.edu>
2019-10-31 16:12:45 -0400

    Started on the State Machine for boss
    - got a state machine for the boss started

---
+++ Assets/Johnson/Scripts/BossStatePursue.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Johnson
+{
+    public class BossStatePursue : BossState
+    {
+        public override BossState Update(BossStateMachine boss)
+        {
+
+            ///////////////////////////// STATE BEHAVIOR: 
+            Vector3 vectorToPlayer = boss.VectorToAttackTarget();
+
+            Vector3 dirToPlayer = vectorToPlayer.normalized;
+
+            boss.transform.position += dirToPlayer * boss.speed * Time.deltaTime;
+
+
+
+            //////////////////////////// TRANSITION:
+            ///
+            if(vectorToPlayer.sqrMagnitude < boss.pursueDistanceThreshold * boss.pursueDistanceThreshold) // if dis < threshold
+            {
+                return new BossStatePursue();
+            }
+
+            if (!boss.CanSeeAttackTarget()) // if we can't see the player..
+            {
+                // transition to idle
+                return new BossStateIdle();
+            }
+
+            return null;
+        }
+    }
+}
