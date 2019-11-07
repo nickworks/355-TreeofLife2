be97505c5e4cdfa4a2fe11afa0db5e150fd73e26
Lauren <whitel24@ferris.edu>
2019-11-07 16:57:05 -0500

    Worked on projectiles

---
+++ Assets/White/Scripts/WhiteProjectileHoming.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace White
+{
+    public class WhiteProjectileHoming : WhiteProjectile
+    {
+        Transform target;
+        private float homingForce = 10000;
+        float maxForce = 1000;
+
+        public void Shoot(GameObject owner, Transform target, Vector3 direction)
+        {
+            this.target = target;
+
+            Shoot(owner, direction);
+        }
+
+        void Update()
+        {
+            GetOlderAndDie();
+
+            Homing();
+
+            transform.rotation = Quaternion.FromToRotation(Vector3.up, body.velocity.normalized);
+        }
+
+        private void Homing()
+        {            
+            Vector3 dir = (target.position - transform.position).normalized;
+
+            Vector3 steer = dir * homingForce - body.velocity;
+            steer = steer.normalized * maxForce;
+
+            body.AddForce(steer * Time.deltaTime);
+        }
+    }
+}
